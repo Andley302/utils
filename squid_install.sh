841d8ba57b2de8721e6a2f3c3f266ce656905b94
Andley S <39422775+Andley302@users.noreply.github.com>
2021-10-30 10:17:58 -0300

    Update squid_install.sh

--- squid_install.sh
+++ squid_install.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
-clear
-echo "Preparing...";
-[[ -d "/etc/squid" ]] && {
-service squid stop;
-apt-get remove squid -y >/dev/null 2>&1
-apt-get purge squid -y >/dev/null 2>&1
-rm -rf /etc/squid >/dev/null 2>&1
-}
-[[ -d "/etc/squid3" ]] && {
-service squid3 stop;
-apt-get remove squid3 -y >/dev/null 2>&1
-apt-get purge squid3 -y >/dev/null 2>&1
-rm -rf /etc/squid3 >/dev/null 2>&1
-apt autoremove -y >/dev/null 2>&1
-}
-clear
-echo "Squid 3.3.8 Installer";
-sleep 3;
-[[ ! -e /etc/apt/sources.list.d/trusty_sources.list ]] && {
-touch /etc/apt/sources.list.d/trusty_sources.list >/dev/null 2>&1
-echo "deb http://us.archive.ubuntu.com/ubuntu/ trusty main universe" | tee --append /etc/apt/sources.list.d/trusty_sources.list >/dev/null 2>&1
-}
-[[ $(grep -wc 'Debian' /etc/issue.net) != '0' ]] && {
-apt install dirmngr -y >/dev/null 2>&1
-[[ $(apt-key list 2>/dev/null | grep -c 'Ubuntu') == '0' ]] && {
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32 >/dev/null 2>&1
-}
-}
-apt update -y >/dev/null 2>&1
-apt install squid3=3.3.8-1ubuntu6 squid=3.3.8-1ubuntu6 squid3-common=3.3.8-1ubuntu6 -y >/dev/null 2>&1
-cd /etc/squid3;
-rm -rf squid.conf;
-wget https://raw.githubusercontent.com/Andley302/utils/main/squid/squid.conf;
-wget https://raw.githubusercontent.com/Andley302/utils/main/squid/payload.txt;
-wget -qO- https://raw.githubusercontent.com/Andley302/utils/main/squid/squid3 >/etc/init.d/squid3
-chmod +x /etc/init.d/squid3 >/dev/null 2>&1
-update-rc.d squid3 defaults >/dev/null 2>&1
-service squid3 restart;
-echo "Done";
 
+#Ask for password if necessary
+sudo echo
 
+#Add Trusty Sources
+sudo touch /etc/apt/sources.list.d/trusty_sources.list
+echo "deb http://us.archive.ubuntu.com/ubuntu/ trusty main universe" | sudo tee --append /etc/apt/sources.list.d/trusty_sources.list > /dev/null
+
+#Update
+sudo apt update
+
+#Install Squid
+sudo apt install -y squid3=3.3.8-1ubuntu6 squid=3.3.8-1ubuntu6 squid3-common=3.3.8-1ubuntu6
+
+#Install missing init.d script
+wget https://raw.githubusercontent.com/Andley302/utils/main/squid/squid3
+sudo cp squid3 /etc/init.d/
+sudo chmod +x /etc/init.d/squid3
+sudo update-rc.d squid3 defaults
+
+#Start squid
+sudo service squid3 start
+
+#Cleanup
+rm squid3
+
+#Print info
+clear
+echo "====================================="
+echo "Squid 3.3.8 is successfully installed!"
+echo "Squid's config is located at '/etc/squid3/squid.conf'"
+echo "You can start/stop/restart squid by using 'sudo service squid3 start/stop/restart'"
+echo "====================================="
