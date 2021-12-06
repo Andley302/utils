1067f75457f92b2e88b696619bf412942d47ebb0
Andley S <39422775+Andley302@users.noreply.github.com>
2021-12-06 20:58:29 -0300

    Update antcrashws.sh

--- antcrashws.sh
+++ antcrashws.sh
@@ -3,6 +3,6 @@ port=$1
 while true
 do
 #python /etc/CrashVPN/wsproxy.py $port
-netstat -tlpn | grep -w 80 > /dev/null || python /etc/CrashVPN/wsproxy.py $port
+  netstat -tlpn | grep -w 80 > /dev/null || python /etc/SSHPlus/wsproxy.py $port
 sleep 1
 done
