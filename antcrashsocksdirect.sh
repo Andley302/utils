517dbe73bae8f803062f104f9fbf174188da47a5
Andley S <39422775+Andley302@users.noreply.github.com>
2022-01-07 18:27:07 -0300

    Update antcrashsocksdirect.sh

--- antcrashsocksdirect.sh
+++ antcrashsocksdirect.sh
@@ -3,6 +3,6 @@ port=$1
 while true
 do
  #python /etc/SSHPlus/proxy.py $port
- netstat -tlpn | grep -w 8080 > /dev/null || python /etc/SSHPlus/wsproxy.py $port
+ netstat -tlpn | grep -w 8080 > /dev/null || python /etc/SSHPlus/proxy.py $port
 sleep 1
 done
