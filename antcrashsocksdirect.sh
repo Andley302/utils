0a72632369b4a78afc85001247cfd0e34b47dd29
Andley S <39422775+Andley302@users.noreply.github.com>
2022-01-07 18:18:50 -0300

    Update antcrashsocksdirect.sh

--- antcrashsocksdirect.sh
+++ antcrashsocksdirect.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
+port=$1
 while true
 do
-ulimit -c unlimited
-ulimit -d unlimited
-ulimit -e unlimited
-ulimit -f unlimited
-ulimit -i unlimited
-ulimit -l unlimited
-ulimit -m unlimited
-ulimit -n 999999
-ulimit -q unlimited
-ulimit -r unlimited
-ulimit -s unlimited
-ulimit -t unlimited
-ulimit -u unlimited
-ulimit -v unlimited
-ulimit -x unlimited
-#python /etc/SSHPlus/proxy.py 8080;
-netstat -tlpn | grep -w 8080 > /dev/null || python /etc/SSHPlus/proxy.py $port
+ python /etc/SSHPlus/proxy.py $port
 sleep 1
 done
