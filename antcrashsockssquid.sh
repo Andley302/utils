a409a859fd89d93b99e61a06ba18f4a349a31dca
Andley S <39422775+Andley302@users.noreply.github.com>
2022-01-07 18:20:38 -0300

    Update antcrashsockssquid.sh

--- antcrashsockssquid.sh
+++ antcrashsockssquid.sh
@@ -2,6 +2,7 @@
 port=$1
 while true
 do
- python /etc/SSHPlus/proxy.py $port
+ #python /etc/SSHPlus/proxy.py $port
+ netstat -tlpn | grep -w 8088 > /dev/null || python /etc/SSHPlus/wsproxy.py $port
 sleep 1
 done
