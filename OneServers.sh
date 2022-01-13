6a34a927d925751f091a04257ba3535dc9bdfdf5
Andley S <39422775+Andley302@users.noreply.github.com>
2022-01-13 10:49:51 -0300

    Update OneServers.sh

--- OneServers.sh
+++ OneServers.sh
@@ -282,8 +282,12 @@ wget https://raw.githubusercontent.com/Andley302/utils/main/limits.conf && chmod
 cd /root;
 clear;
 cd /root && wget https://raw.githubusercontent.com/Andley302/utils/main/badvpn/install_badvpn.sh && chmod +x install_badvpn.sh && ./install_badvpn.sh;
-
+clear;
+echo "INSTALANDO FAST";
+wget https://github.com/ddo/fast/releases/download/v0.0.4/fast_linux_amd64
+sudo install fast_linux_amd64 /usr/local/bin/fast;
 
 ##FIM
+clear;
 echo "Fim!";
 sleep 5;
